c9b26a2e4732eac7a6924af7542c0833a39e6ec8
Cang-cang Zhang <41459750+CmosZhang@users.noreply.github.com>
2019-12-25 14:55:38 +0800

    Add files via upload

---
+++ LintCode/KthSmallestElementInASortedMatrix1272.java
@@ -0,0 +1,10 @@
+package LintCode;
+
+public class KthSmallestElementInASortedMatrix1272
+{
+    public int kthSmallest(int[][] matrix, int k) {
+        // write your code here
+    	int n = matrix.length;
+    }
+
+}
