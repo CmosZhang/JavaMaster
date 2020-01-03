589a43dd312e22d76f6c1839023d1e2678ee9efa
Cang-cang Zhang <41459750+CmosZhang@users.noreply.github.com>
2020-01-03 16:06:27 +0800

    Add files via upload

--- LintCode/MaxChunksToMakeSorted1039.java
+++ LintCode/MaxChunksToMakeSorted1039.java
@@ -1,5 +1,7 @@
 package LintCode;
 
+import java.util.Stack;
+
 public class MaxChunksToMakeSorted1039
 {
 	// 1039. 最大分块排序
@@ -27,13 +29,32 @@ public class MaxChunksToMakeSorted1039
 		return res + 1;
 	}
 	
+	
 	//1040. 最大分块排序 II
+	//单调栈是有问题的，例如例子 arr={1,3,2,5,4,4,7,6,6,8},
+	//虽然通过测试案例，但是方法有问题，最好还是利用和1039题一样的方法
 	public int maxChunksToSorted2(int[] arr) {
 		// Write your code here
 		if (arr == null || arr.length == 0) {
 			return 0;
 		}
-		
+		int len = arr.length;
+		Stack<Integer> stk = new Stack<Integer>();
+		stk.push(arr[0]);
+		for (int i = 1; i < len; i++) {
+			if (arr[i] >= stk.peek()) {
+				stk.push(arr[i]);
+			}
+			else {
+				int temp = stk.peek();
+				stk.pop();
+				while (!stk.empty() && stk.peek() < temp) {
+					stk.pop();
+				}
+				stk.push(temp);
+			}
+		}
+		return stk.size();
 	}
 
 }
